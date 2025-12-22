03b1a36fb7b5d7fcc13f2eacca0243c63afcd0d3
UDAY-HA1210 <udayha1210@gmail.com>
2025-12-22 23:30:18 +0530

    Added SydantekVer4.1_Workspace1p0 firmware

---
+++ SydantekVer4.1_Workspace1p0/application/Release/Application/subdir_vars.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Application/haldi.c \
+C:/ti/simplelink_cc13x2_26x2_sdk_4_30_00_54/source/ti/ble5stack/common/cc26xx/util.c 
+
+C_DEPS += \
+./Application/haldi.d \
+./Application/util.d 
+
+OBJS += \
+./Application/haldi.obj \
+./Application/util.obj 
+
+OBJS__QUOTED += \
+"Application\haldi.obj" \
+"Application\util.obj" 
+
+C_DEPS__QUOTED += \
+"Application\haldi.d" \
+"Application\util.d" 
+
+C_SRCS__QUOTED += \
+"../Application/haldi.c" \
+"C:/ti/simplelink_cc13x2_26x2_sdk_4_30_00_54/source/ti/ble5stack/common/cc26xx/util.c" 
+
+
