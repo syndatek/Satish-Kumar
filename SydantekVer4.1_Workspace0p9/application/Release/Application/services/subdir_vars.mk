597e8cf277487035d16293aabde74641fc53ee29
UDAY-HA1210 <udayha1210@gmail.com>
2025-12-22 23:29:35 +0530

    Added SydantekVer4.1_Workspace0p9 firmware

---
+++ SydantekVer4.1_Workspace0p9/application/Release/Application/services/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Application/services/sydantek.c 
+
+C_DEPS += \
+./Application/services/sydantek.d 
+
+OBJS += \
+./Application/services/sydantek.obj 
+
+OBJS__QUOTED += \
+"Application\services\sydantek.obj" 
+
+C_DEPS__QUOTED += \
+"Application\services\sydantek.d" 
+
+C_SRCS__QUOTED += \
+"../Application/services/sydantek.c" 
+
+
