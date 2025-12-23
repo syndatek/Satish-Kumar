74c0fce10e130ed69948d670968de599a562a50d
UDAY-HA1210 <udayha1210@gmail.com>
2025-12-23 09:32:59 +0530

    Added SydantekVer4.1_Workspace1p2-watchdog firmware

---
+++ SydantekVer4.1_Workspace1p2-watchdog/application/Release/Application/services/subdir_vars.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Application/services/sydantek.c \
+../Application/services/watchdog.c 
+
+C_DEPS += \
+./Application/services/sydantek.d \
+./Application/services/watchdog.d 
+
+OBJS += \
+./Application/services/sydantek.obj \
+./Application/services/watchdog.obj 
+
+OBJS__QUOTED += \
+"Application\services\sydantek.obj" \
+"Application\services\watchdog.obj" 
+
+C_DEPS__QUOTED += \
+"Application\services\sydantek.d" \
+"Application\services\watchdog.d" 
+
+C_SRCS__QUOTED += \
+"../Application/services/sydantek.c" \
+"../Application/services/watchdog.c" 
+
+
